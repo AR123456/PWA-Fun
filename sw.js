74fff63314c99eaa1e865968e241e4aeaf24ef6e
AR123456 <coloradoz99@gmail.com>
2020-11-13 07:29:54 -0700

    SW active event

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-// install event
-// self here, inside of service worker refers to the service worker
-// evt is the event 
-// when this occurs it is going to fire a callback funciton 
-// this callback function takes as a paramiter 
-// an event object that represent the install event 
-
-self.addEventListener('install', evt => {
+// listing for the activate event inside the service worker 
+ self.addEventListener('install', evt => {
   // could add add assets to the cash here for use in offline mode
   // things that do not change much like the main css file, logo, index page ect
   console.log('service worker installed');
 });
+
+// listen for activate service worker - browser will not automaticaly reactive and replace
+ 
+self.addEventListener("activate", evt=>{
+
+
+  console.log("servicer just got activated ")
+
+})
+
+// skip waiting or activate on relload as a devloper 
