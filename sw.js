8a642644bd28fe2845d61ca0e79b0b93aaf8a2c6
AR123456 <coloradoz99@gmail.com>
2020-11-14 06:02:48 -0700

    fetch events

--- sw.js
+++ sw.js
@@ -1,17 +1,15 @@
-// listing for the activate event inside the service worker 
+// install service worker 
  self.addEventListener('install', evt => {
-  // could add add assets to the cash here for use in offline mode
-  // things that do not change much like the main css file, logo, index page ect
-  console.log('service worker installed');
+   console.log('service worker installed');
 });
 
-// listen for activate service worker - browser will not automaticaly reactive and replace
+// activate event
  
 self.addEventListener("activate", evt=>{
-
-
   console.log("servicer just got activated ")
-
 })
 
-// skip waiting or activate on relload as a devloper 
+ // fetch event 
+ self.addEventListener("fetch",evt=>{
+   console.log("fetch just happened",evt)
+ })
