ccfe0371406022dd4b30b66400cdc2c6a0798ebe
AR123456 <coloradoz99@gmail.com>
2020-11-19 05:29:14 -0700

    updating the ui

--- js/db.js
+++ js/db.js
@@ -1,23 +1,12 @@
-// here is the js to communicate with the db add a script tag in index for this
-// here we want to communicate with the recipes collection, use collection() method
-////// real time listener
-// set up real time listioner to the database - so we know when evers something changes
-// use the onSnapshot() method , it takes a callback
 db.collection("recipes").onSnapshot((snapshot) => {
-  // liston for change and when there is when send a snapshot of the collection to me in this
-  // call back
   // console.log(snapshot.docChanges());
-  // cycle throught the changes and output the change
-  // array of document changes
-  // .forEach()  will cycle through and perform a function on each item in the array
-  // call back takes the individual object for that iteration
   snapshot.docChanges().forEach((change) => {
-    // this gets us the data object of this change
     // console.log(change, change.doc.data(), change.doc.id);
-    // look at the type of each change, if something is added
-    // output to the dom, if removed do not add to the dom
+
     if (change.type === "added") {
-      // add the document data to the web page
+      // call a function that will update our UI with data from the DOM
+      // we need the doc.id() to track changes - this has to do with UI so define function in ui.js
+      renderRecipe(change.doc.data(), change.doc.id);
     }
     if (change.type === "removed") {
       // remove the document data from the web page
